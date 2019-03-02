0a6a6c7eaca66e809d6ddf344a1e7eecb7b42926
Claus Denk <denk@us.es>
2019-03-02 15:08:47 +0100

    job automation aws

--- example_job.py
+++ example_job.py
@@ -5,13 +5,14 @@ import numpy as np
 
 from preprocessing.make_keras_input import data
 from models import M16
+from keras import backend as K
 from keras.optimizers import Adam 
 
 
 
 def load_model_weights(name, model):
     try:
-        model.load_weights('weights/M16-hiragana_weights.h5')
+        model.load_weights('weights/weights_in.h5')
     except:
         print("Can't load weights!")
 
@@ -23,18 +24,29 @@ def save_model_weights(name, model):
         print("failed to save classifier weights")
     pass
 
+img_rows, img_cols = 64, 64
 X_train, y_train, X_test, y_test = data(mode='hiragana')
 n_output = y_train.shape[1]
-print("data read")
-model = M16(n_output=n_output, input_shape=(1, 64, 64))
 
-load_model_weights('weights/M16-hiragana_weights.h5', model)
+if K.image_dim_ordering() == 'th':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+print ("using shape",input_shape)
+model = M16(n_output=n_output, input_shape=input_shape)
+
+load_model_weights('weights/weights_in.h5', model)
 
 adam = Adam(lr=1e-4)
 model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
 model.fit(X_train, y_train,
-          epochs=20,
+          epochs=1,
           batch_size=16)
 
 score, acc = model.evaluate(X_test, y_test,
@@ -44,4 +56,4 @@ print ("Training size: ", X_train.shape[0])
 print ("Test size: ", X_test.shape[0])
 print ("Test Score: ", score)
 print ("Test Accuracy: ", acc)
-save_model_weights('weights/M16-hiragana_weights.h5', model)
+save_model_weights('weights/weights_out.h5', model)
---
+++ run_job.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# This script runs the complete learning job
+#
+# names of files in s3 bucket:
+export WEIGHTS_IN=M16-hiragana_weights.h5
+export WEIGHTS_OUT=M16-hiragana_weights_out.h5
+
+# on aws set environment and get data from s3
+if [[ "$HOSTNAME" == "ip-"* ]]; then
+    source activate tensorflow_p36
+
+    # get ETLC data
+    aws s3 cp "s3://clausdata/ETLC.zip" ETLC.zip
+    if [ $? -eq 0 ]; then
+        echo "Error getting ETLC.zip from s3"
+        exit 1
+    fi
+    unzip ETLC.zip
+
+    # make weights dir
+    mkdir -p weights
+    # copy weights from s3 if they exist
+    aws s3 ls "s3://clausdata/{$WEIGHTS_IN}"
+    if [ $? -eq 0 ]; then
+        echo "Using existing weights ${WEIGHTS_IN} from s3"
+        aws s3 cp "s3://clausdata/${WEIGHTS_IN}" weights/weights_in.h5
+    else
+        echo "Weights ${WEIGHTS_IN} not found on s3"
+    fi
+fi
+# run job
+RES=$(python example_job.py &> output.txt)
+# on aws ..
+if [[ "$HOSTNAME" == "ip-"* ]]; then
+    aws s3 cp run_job.out s3://clausdata/run_job.out 
+    aws s3 cp output.txt s3://clausdata/output.txt 
+    # save to s3 if terminated normally
+    if [ "$RES" -eq 0 ]; then
+        echo "saving weights ${WEIGHTS_OUT} to s3..."
+        aws s3 cp weights/weights_out.h5 "s3://clausdata/${WEIGHTS_OUT}" 
+    else
+        echo "Error running job, check output.txt on s3"
+    fi
+    # stop (and possibly terminate) instance
+    sudo halt
+fi
