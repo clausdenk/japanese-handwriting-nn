cf8b8e04ea5bb45402fc8f696707d223ff428fe6
Claus Denk <denk@us.es>
2019-03-02 16:05:45 +0100

    aws batch job

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+# This is just a wrapper to execute run_job.sh so that we can save output to s3
 ./run_job.sh &> run_job.out.txt
 if [[ "$HOSTNAME" == "ip-"* ]]; then
     aws s3 cp run_job.out.txt s3://clausdata/run_job.out 
