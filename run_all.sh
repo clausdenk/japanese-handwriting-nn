86901f10ba5f840255744ab6b7097e61ea9fae03
Claus Denk <denk@us.es>
2019-03-03 20:23:39 +0100

    prepare batch job

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This is just a wrapper to execute run_job.sh so that we can save output to s3
+#
 export S3_BUCKET=s3://clausdata  
 ./run_job.sh &> run_job.out.txt
 if [[ $(hostname) == "ip-"* ]]; then
