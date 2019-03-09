59180aca37f3c5516fbcab501e36ff0e46d5d231
Claus Denk <denk@us.es>
2019-03-09 21:35:38 +0100

    change name

--- load_mobile.py
+++ load_mobile.py
@@ -24,19 +24,7 @@ model = MobileNet(classes=n_output, input_shape=input_shape)
 
 load_model_weights('weights/weights_in.h5', model)
 
-# adam = Adam(lr=1e-4)
-# model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
-
-# model.summary()
-
-# model.fit(X_train, y_train, epochs=5, batch_size=16) #  verbose=2 
-
-
-score, acc = model.evaluate(X_test, y_test, batch_size=16, verbose=0)
-
 print ("Training size: ", X_train.shape[0])
 print ("Test size: ", X_test.shape[0])
-print ("Test Score: ", score)
-print ("Test Accuracy: ", acc)
 
 
